6362400bc1847ae34d14c37cc48328619507c9e4
vidya <vidya136@gmail.com>
2019-07-18 15:55:19 -0700

    jenkins:

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ class TestCalculator(unittest.TestCase):
         assert 4 == calculator.add(2, 2)
 
     def test_subtraction(self):
-        assert 0 == calculator.subtract(4, 4)
+        assert 2 == calculator.subtract(4, 2)
 
 
 if __name__ == '__main__':
